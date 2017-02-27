87081b3f5452f12f83e695e3fe199ef51cd7df95
David Jobe <daavid.jobe@gmail.com>
2017-02-27 20:13:39 +0100

    sliding animation

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { RippleButton } from '../ripplebutton/RippleButton'
 import Arrow from '../arrow/Arrow'
 import './styles.scss'
@@ -9,6 +9,16 @@ export class Dropdown extends Component {
     isOpen: false
   }
 
+  static propTypes = {
+    arrowColor: PropTypes.string,
+    elementCass: PropTypes.string
+  }
+
+  static defaultProps = {
+    arrowColor: '#fff',
+    elementClass: ''
+  }
+
   handleClick = () => {
     const { isOpen } = this.state
     this.setState({ isOpen: !isOpen })
@@ -17,16 +27,27 @@ export class Dropdown extends Component {
   render () {
     const { isOpen } = this.state
     return (
-      <div className='awsm-dropdown'>
+      <div className={`awsm-dropdown ${this.props.elementCass}`}>
         <div className='dropdown-btn'>
           <RippleButton onClick={this.handleClick}>
             <div>Dropdown</div>
             <Arrow
               elementClass={`arrow-${isOpen ? 'up' : 'down'}`}
               width={25}
-              color='#fff'
+              color={this.props.arrowColor}
             />
           </RippleButton>
+        </div>
+        <div className={`dropdown-list ${isOpen ? 'slide-down' : 'slide-up'}`}>
+          <ul>
+            <li>item1</li>
+            <li>item2</li>
+            <li>item3</li>
+            <li>item3</li>
+            <li>item3</li>
+            <li>item3</li>
+            <li>item3</li>
+          </ul>
         </div>
       </div>
     )
